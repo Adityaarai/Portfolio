3ace5c01f93114248be49984a2a59f37cb87f72e
AdityaRai <adityarai23ar@gmail.com>
2023-02-04 21:15:43 +0545

    completed portfolio responsive left

--- Portfolio/js/portfolio.js
+++ Portfolio/js/portfolio.js
@@ -1,12 +1,29 @@
-var lastScrollTop;
-navbar = document.getElementById('navbar');
-window.addEventListener('scroll',function(){
-var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-if(scrollTop > lastScrollTop){
-navbar.style.top='-80px';
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("activelink");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("activetab");
+    }
+    event.currentTarget.classList.add("activelink");
+    document.getElementById(tabname).classList.add("activetab");
 }
-else{
-navbar.style.top='0';
-}
-lastScrollTop = scrollTop;
+//disappearing about section
+
+let navi = document.querySelector(".navbar");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", function(){
+    if (lastScrollY < window.scrollY){
+        navi.classList.add("nav-hide")
+    } else {
+        navi.classList.remove("nav-hide")
+    }
+
+    lastScrollY = window.scrollY
 });
+// disappearing navbar
+
