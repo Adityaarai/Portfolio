596217eb6d274089ecd2f8f4752fb2cb795a5a9a
AdityaRai <adityarai23ar@gmail.com>
2023-02-10 01:14:51 +0545

    made new css sheet and made website responsive

--- Portfolio/js/portfolio.js
+++ Portfolio/js/portfolio.js
@@ -13,10 +13,34 @@ function opentab(tabname){
 }
 //disappearing about section
 
-let navi = document.querySelector(".navbar");
-let lastScrollY = window.scrollY;
+var smenu = document.getElementById("menu");
 
-window.addEventListener("scroll", function(){
+function openmenu(){
+    smenu.style.right = "0";
+}
+function closemenu(){
+    smenu.style.right = "-400px";
+}
+
+if ( screen.width > 1024) {
+    let navi = document.querySelector(".navbar");
+    let lastScrollY = window.scrollY;
+
+    window.addEventListener("scroll", function(){
+    if (lastScrollY < window.scrollY){
+        navi.classList.add("nav-hide")
+        console.log(screen.width)
+    } else {
+        navi.classList.remove("nav-hide")
+    }
+    lastScrollY = window.scrollY
+    });
+}
+else {
+    let navi = document.querySelector(".navbar.not(#menu)");
+    let lastScrollY = window.scrollY;
+
+    window.addEventListener("scroll", function(){
     if (lastScrollY < window.scrollY){
         navi.classList.add("nav-hide")
     } else {
@@ -24,15 +48,6 @@ window.addEventListener("scroll", function(){
     }
     lastScrollY = window.scrollY
     });
-
-var smenu = document.getElementById("menu");
-
-function openmenu(){
-    smenu.style.right = "0";
 }
-function closemenu(){
-    smenu.style.right = "-200px";
-}
-
 // disappearing navbar
 
