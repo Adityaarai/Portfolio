98a61d535ee8e65d0c532644736f1180972b8c74
AdityaRai <adityarai23ar@gmail.com>
2023-02-11 16:00:49 +0545

    made some quality changes

--- js/index.js
+++ js/index.js
@@ -26,10 +26,17 @@ function closemenu(){
 //contact form  
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyP2KXY8BcBQrj4A78sMfM_-nKA1km-gTUuowzB6ztTurAUVmlYGeBwecF4-TC1ozmS/exec'
 const form = document.forms['Contact-Me']
+let submit = document.querySelector(".submit")
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+  submit.value = "Submitting..";
+  submit.disabled = true;
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert('Success! Your message has been submitted.', response))
+    .then(response => alert('Your response has been submitted!', response))
+    .then(data => {
+        submit.value = "Submit";
+        submit.disabled = false;
+    })
     .catch(error => console.error('Error!', error.message))
 })
